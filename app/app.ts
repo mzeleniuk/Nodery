8b8d0b90f65c1f18a06e7ba18922e9cf26f1d10c
Michael Zeleniuk <zell.xp@gmail.com>
2020-05-13 16:45:03 +0300

    Add a Winston logger

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
-import * as http from "http";
+import http from "http";
+import winston from "winston";
+import expressWinston from "express-winston";
 
 import { CommonRoutesConfig } from "./common/common.routes.config";
 import { AuthRoutes } from "./auth/auth.routes.config";
@@ -9,12 +11,51 @@ const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port: number = 3000;
 const routes: Array<any> = [];
+const index: number = expressWinston.requestWhitelist.indexOf("headers");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (index !== -1) {
+  expressWinston.requestWhitelist.splice(index, 1);
+}
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).send();
+  } else {
+    return next();
+  }
+});
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+}));
+
 routes.push(new AuthRoutes(app));
 routes.push(new UsersRoutes(app));
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  )
+}));
 
 app.get("/", (request: express.Request, response: express.Response) => {
   response.status(200).send(`Server running at port ${port}`);
